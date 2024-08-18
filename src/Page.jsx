3129004d3909a219404ac3928893e04c44cec737
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-08-18 14:03:10 +0800

    Project setup

---
+++ src/Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <main className='bg-violet-'>
+            <article>
+                <section>Hello World</section>
+            </article>
+        </main>
+    )
+}
+
+export default Page

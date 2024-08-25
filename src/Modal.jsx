29474da540ed130906b501ab1bd934bc469d4331
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-08-25 16:27:15 +0800

    Style: Added animation

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -6,8 +6,8 @@ import Krisha from "./images/krisha.jpg"
 const Modal = ({ close }) => {
     return (
         <>
-            <section className='absolute top-0 h-full w-full bg-black/60 flex justify-center items-center p-8'>
-                <article className='bg-pink-primary h-110 w-130 rounded-lg shadow-xl p-7 flex flex-col gap-3 max-lg:w-[90%] max-md:w-full max-md:h-full'>
+            <section className='absolute top-0 h-full w-full bg-black/60 flex justify-center items-center p-8 overflow-hidden'>
+                <article className='animate-reversesliding bg-pink-primary h-110 w-130 rounded-lg shadow-xl p-7 relative flex flex-col gap-3 max-lg:w-[90%] max-md:w-full max-md:h-full'>
                     <aside className='bg-green-primary p-2 rounded-md self-end border-2 border-black cursor-pointer hover:bg-green-300' onClick={close}>
                         <IoCloseOutline className='text-3xl'/>
                     </aside>
--- src/Page.jsx
+++ src/Page.jsx
@@ -6,12 +6,12 @@ const Page = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <main>
+        <main className='overflow-hidden'>
             <article className='h-100 bg-violet-secondary flex justify-center items-center'>
-                <section className='text-8xl text-white font-qwitcher-grypen font-[700] drop-shadow-md text-center px-8'>Happy Birthday Krisha!</section>
+                <section className='animate-sliding text-8xl text-white font-qwitcher-grypen font-[700] drop-shadow-md text-center px-8 relative'>Happy Birthday Krisha!</section>
             </article>
-            <aside className='h-[26.95rem] bg-violet-primary grid place-items-center'>
-                <button className='font-jost text-2xl bg-blue-300 h-24 w-64 rounded-full animation border-4 border-[#304674]  shadow-thick relative active:top-[15px] active:shadow-none' onClick={() => {setToggle(true)}}>Click me</button>
+            <aside className='h-[26.95rem] bg-violet-primary flex justify-center items-center'>
+                <button className='animate-reversesliding font-jost text-2xl bg-blue-300 h-24 w-64 rounded-full animation border-4 border-[#304674]  shadow-thick relative active:top-[15px] active:shadow-none' onClick={() => {setToggle(true)}}>Click me</button>
             </aside>
             {toggle && (
                 <Modal 
